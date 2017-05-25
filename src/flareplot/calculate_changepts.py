f9193facde540fe90ddf020da3fd77ee1eb759dc
Anthony <akma327@stanford.edu>
2017-05-25 15:45:32 -0700

    Working implementation of computing changepoints from time-series network graph

--- src/flareplot/calculate_changepts.py
+++ src/flareplot/calculate_changepts.py
@@ -1,7 +1,9 @@
 import os
 import sys
-import numpy
+import numpy as np
 import json
+import ast
+import time
 from SIMPLEchangepoint import ComputeChanges
 
 
@@ -12,10 +14,11 @@ USAGE_STR = """
 # Output: Changepoints regarding how the network evolves over time
 
 # Usage
-# python calculate_changepts.py <INPUT_JSON>
+# python calculate_changepts.py <INPUT_JSON> <-lambda>
 
 # Arguments
 # <INPUT_JSON> Absolute path to the input flareplot json 
+# <-lambda> Optional penalization parameter. Default 0.1
 
 # Example
 INPUT_JSON="/scratch/PI/rondror/akma327/DynamicNetworks/data/crystal-analysis/interaction-footprint-analysis/simulation-evobundle/flareplot-dataset/water-network/M2R-active-restrained-unpublished-rep_1.json"
@@ -26,11 +29,14 @@ python calculate_changepts.py $INPUT_JSON
 
 K_MIN_ARG = 2
 
-def calc_cpts(INPUT_JSON):
+def calc_cpts(INPUT_JSON, LAMBDA=0.1):
     """
     Calculates the changepoints and associated time-series observables 
     associated with a particular change in evolving graph network.
     """
+
+
+    ### Process time-series network graph 
     f = open(INPUT_JSON, 'r')
     json_str = ""
     for line in f:
@@ -38,7 +44,46 @@ def calc_cpts(INPUT_JSON):
 
     flareplot = json.loads(json_str)
     edges = flareplot["edges"]
-    print(len(edges))
+
+    edge_to_tppts = {}
+    max_frame = 0
+    for e in edges:
+        name1, name2 = str(e['name1']), str(e['name2'])
+        if("-" in name1 or "-" in name2): continue
+        frames = e['frames']
+        mf = max(frames)
+        if(mf > max_frame):
+            max_frame = mf 
+        key = (name1, name2)
+        edge_to_tppts[key] = frames
+
+    ### Generate the JxT dataframe where J = time series observables, T = number of time points
+    data = []
+    row_labels = []
+    for key in edge_to_tppts:
+        row_labels.append(key)
+        row_info = np.zeros((max_frame + 1))
+        row_info[edge_to_tppts[key]] = 1
+        data.append(list(row_info))
+
+    data = np.array(data)
+
+    changepoints = ComputeChanges(data, LAMBDA)
+    return changepoints, row_labels
+
+
+def driver(INPUT_JSON, LAMBDA):
+    changepoints, row_labels = calc_cpts(INPUT_JSON, LAMBDA)
+
+    for cp_time in changepoints:
+        variables = list(changepoints[cp_time])
+        n1, n2 = row_labels[variables[0]]
+        var_str = n1 + "--" + n2
+        for i, v in enumerate(variables):
+            if(i> 0):
+                n1, n2 = row_labels[variables[i]]
+                var_str += ", " + n1 + "--" + n2
+        print("Time: " + str(cp_time) + ";\t\t\tVariables: " + var_str)
 
 
 
@@ -48,5 +93,9 @@ if __name__ == "__main__":
         exit(1)
 
     (INPUT_JSON) = (sys.argv[1])
-    calc_cpts(INPUT_JSON)
+    LAMBDA = 0.1
+    if("-lambda" in sys.argv):
+        LAMBDA = sys.argv[sys.argv.index("-lambda") + 1]
+
+    driver(INPUT_JSON, LAMBDA)
         
